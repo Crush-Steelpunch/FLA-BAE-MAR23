2378e410cabb6aa20dd7548da6689cdb8341ccf3
Leon L. Robinson <leon.robinson@qa.com>
2023-04-03 11:23:36 +0100

    Correct example

--- class-instruction.py
+++ class-instruction.py
@@ -54,27 +54,27 @@ print(thisisanobject.subnum(7))
 
 # Passing data between instances of objects
 
-class Nummaths2:
+class Vis:
+        
+        def __init__(self, invar,mydata):
+                    self.invar = invar
+                    self.mydata = mydata
+                                
+        def showme(self):
+            print(self)
 
-    def __init__(self,setnum):
-        self.mathvar = setnum        
+        def copytome(self,objectin):
+            print(objectin)
+            self.mydata = objectin.invar
+                                                                            
 
-    def addnum(self, invar):
-        return invar + self.mathvar
+object1 = Vis("this","")
+print(object1)
+object1.showme()
+object2 = Vis("that","")
+print(object2)
+object2.showme()
 
-    def subnum(self, invar):
-        return invar - self.mathvar
-
-    
-mathvar = Nummaths2()
-anothervar = Nummaths2(10)
-thisisanobject = Nummaths2(30)
-
-
-print(mathvar.addnum(7))
-print(mathvar.subnum(7))
-print(anothervar.addnum(7))
-print(anothervar.subnum(7))
-print(thisisanobject.addnum(7))
-print(thisisanobject.subnum(7))
+object2.copytome(object1)
+print(object2.mydata
 
