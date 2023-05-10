a1988331855bb08db3cbcc792871763092fb9c01
Leon L. Robinson <leon.robinson@qa.com>
2023-05-10 15:51:40 +0100

    Added my first response function

--- API/app.py
+++ API/app.py
@@ -2,7 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
+@app.route('/')
+def main_homepage_function():
+    return "This is my python app response"
 
 if __name__=='__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
