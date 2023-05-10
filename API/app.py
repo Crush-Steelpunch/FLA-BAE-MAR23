a0ec7062b334b422d7d9b587462b3caec5683db7
Leon L. Robinson <leon.robinson@qa.com>
2023-05-10 15:53:53 +0100

    added a more complex output

--- API/app.py
+++ API/app.py
@@ -4,7 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def main_homepage_function():
-    return "This is my python app response"
+    htmloutvar = ''
+    for i in range(7):
+        htmloutvar = htmloutvar + "This is line " + i + "\n"
+    return htmloutvar
+
+    return 
 
 if __name__=='__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
