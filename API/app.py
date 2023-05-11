ab64b0e54763f4f2ce5493d14e8046a06c23d127
Leon L. Robinson <leon.robinson@qa.com>
2023-05-11 09:46:11 +0100

    change on input

--- API/app.py
+++ API/app.py
@@ -2,6 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
+recipie1 = "Recipe Name: Spicy Chicken and Rice Bowl<br>Nutrition (per serving): Calories: 590, Carbs: 75g, Protein: 30g, Fat: 18g<br>Ingredients:<br>- 2 boneless, skinless chicken breasts<br>"
+recipie2 = "Recipe Name: Quick Chickpea Salad<br>Nutrition: Calories:  300, Total Fat:  10g, Carbohydrates: 30g, Protein: 10g "
+
 @app.route('/')
 def main_homepage_function():
     htmloutvar = ''
@@ -9,7 +12,16 @@ def main_homepage_function():
         htmloutvar = htmloutvar + "This is line " + str(i) + "<br>"
     return htmloutvar
 
-    return 
+@app.route('/recipies/<recipieid>')
+def recipieshow(recipieid):
+    if recipieid == 1:
+        return recipie1
+    elif recipieid == 2:
+        return recipie2
+    else:
+        return "Not yet input"
+
+@app.route('/toothbrushes')
 
 if __name__=='__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
