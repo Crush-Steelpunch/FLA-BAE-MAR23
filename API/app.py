dc13cafe986d44bb01493d0a7d98b1302d217eb8
Leon L. Robinson <leon.robinson@qa.com>
2023-05-11 10:56:51 +0100

    fix unknown keyword

--- API/app.py
+++ API/app.py
@@ -26,9 +26,9 @@ def recipieshow(recipieid):
     else:
         return recipiedb[int(recipieid)]
 
-@app.route('/delrecipie/<recipieid>', method=['GET','POST'])
+@app.route('/delrecipie/<recipieid>', methods=['GET','DELETE'])
 def recipiedel(recipieid):
-    if request.method == 'POST':
+    if request.method == 'DELETE':
         recipiedb.pop(int(recipieid))
     else:
         return "Here is all the recipies: <br>"  + recipiedb + "<br> You need to use a DELETE http method"
