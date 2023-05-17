46a0060324186e42bfb2781b398cd50c9ba2e8cc
Leon L. Robinson <leon.robinson@qa.com>
2023-05-17 10:29:12 +0100

    keep default sec group

--- Terraform/example1/main.tf
+++ Terraform/example1/main.tf
@@ -15,5 +15,5 @@ resource "aws_instance" "this" {
   ami                     = "ami-09744628bed84e434"
   key_name                = "fla-may15"
   instance_type           = "t2.micro"
-  vpc_security_group_ids  =  ["sg-0851886f344803422" ]
+  vpc_security_group_ids  =  ["sg-0851886f344803422","sg-0b1e75c9741f11704" ]
 }
