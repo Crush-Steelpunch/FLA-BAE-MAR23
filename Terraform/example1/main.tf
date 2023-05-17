21a63afbe2a77f951305e8a7880b29ed99487e2b
Leon L. Robinson <leon.robinson@qa.com>
2023-05-17 11:08:39 +0100

    autoformat

--- Terraform/example1/main.tf
+++ Terraform/example1/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws"
     }
   }
 }
@@ -12,8 +12,8 @@ provider "aws" {
 }
 
 resource "aws_instance" "this" {
-  ami                     = "ami-09744628bed84e434"
-  key_name                = "fla-may15"
-  instance_type           = "t2.micro"
-  vpc_security_group_ids  =  ["sg-0851886f344803422","sg-0b1e75c9741f11704" ]
+  ami                    = "ami-09744628bed84e434"
+  key_name               = "fla-may15"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = ["sg-0851886f344803422", "sg-0b1e75c9741f11704"]
 }
