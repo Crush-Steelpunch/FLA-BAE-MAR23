ffb16a1b2b5cd84728b88eac721551753d97a231
Leon L. Robinson <leon.robinson@qa.com>
2023-05-17 11:33:37 +0100

    Bringo your own secgroup

--- Terraform/example1/main.tf
+++ Terraform/example1/main.tf
@@ -15,5 +15,32 @@ resource "aws_instance" "this" {
   ami                    = "ami-09744628bed84e434"
   key_name               = "fla-may15"
   instance_type          = "t2.micro"
-  vpc_security_group_ids = ["sg-0851886f344803422", "sg-0b1e75c9741f11704"]
+  vpc_security_group_ids = [aws_security_group.allow_ssh, "sg-0b1e75c9741f11704"]
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow ssh inbound traffic"
+  vpc_id      = "vpc-0f03c543cd14b23ac"
+
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
 }
